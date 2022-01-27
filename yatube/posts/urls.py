66f62ed2046e2508e31b4ad9c3ee59c62ef97e35
Александр Горыничев <px2005@yandex.ru>
2022-01-27 15:58:32 +0300

    Рабочая версия v1.01

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,13 +2,15 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
     # Главная страница
-    path('', views.index),
+    path('', views.index, name='index'),
     # Страница со списком
-    path('posts/', views.posts_list),
+    path('posts/', views.posts_list, name='posts_list'),
     # Отдельная страница с информацией
-    path('posts/<pk>/', views.posts_detail),
-    path('group/', views.group),
-    path('group/<slug:slug>/', views.group_posts),
+    # path('posts/<pk>/', views.posts_detail),
+    path('group/', views.group_posts, name='group'),
+    path('group/<slug:slug>/', views.group_posts, name='group_list'),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,27 +1,66 @@
-from django.shortcuts import render
-
-# Create your views here.
-# posts/views.py
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 # Главная страница
 def index(request):
-    return HttpResponse('Главная страница')
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+        # В словарь можно передать переменную
+        'title': 'Последние обновления на сайте',
+        # А можно сразу записать значение в словарь. Но обычно так не делают
+        'text': 'Главная страница',
+    }
+    return render(request, 'posts/index.html', context)
 
 
 # Страница со списком мороженого
 def posts_list(request):
-    return HttpResponse('Список мороженого')
+    context = {
+        # В словарь можно передать переменную
+        'title': 'Это главная страница проекта Yatube',
+        # А можно сразу записать значение в словарь. Но обычно так не делают
+        'text': 'Главная страница',
+    }
+    return render(request, 'posts/index.html', context)
 
 
-# Страница с информацией об одном сорте мороженого;
-# view-функция принимает параметр pk из path()
-def posts_detail(request, pk):
-    return HttpResponse(f'Мороженое номер {pk}')
+def group(request, slug):
+    # Функция get_object_or_404 получает по заданным критериям объект
+    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
+    # В нашем случае в переменную group будут переданы объекты модели Group,
+    # поле slug у которых соответствует значению slug в запросе
+    group = get_object_or_404(Group, slug=slug)
 
-def group(request):
-    return HttpResponse('Список group')
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
+
 
 def group_posts(request, slug):
-    return HttpResponse(f'group_posts {slug}')
+    # Функция get_object_or_404 получает по заданным критериям объект
+    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
+    # В нашем случае в переменную group будут переданы объекты модели Group,
+    # поле slug у которых соответствует значению slug в запросе
+    group = get_object_or_404(Group, slug=slug)
+
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
